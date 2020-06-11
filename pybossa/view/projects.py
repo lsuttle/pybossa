612b0921c9068f7f58b9b4d59e9b683de43aacc0
ywrsusan <wyuan35@bloomberg.net>
2020-06-11 17:29:21 -0400

    just test build

--- pybossa/view/projects.py
+++ pybossa/view/projects.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PYBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 
+
+
+
 import time
 import re
 import json
