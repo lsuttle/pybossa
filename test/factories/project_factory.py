7daab96aa833797a388cb591a4934d58764aa03c
Laura Suttle <laura.suttle@gmail.com>
2020-07-07 19:22:01 -0400

    adding kpi to factory to fix test

--- test/factories/project_factory.py
+++ test/factories/project_factory.py
@@ -31,6 +31,8 @@ class ProjectFactory(BaseFactory):
             project.set_password('hello')
         if 'data_classification' not in project.info:
             project.info['data_classification'] = dict(input_data="L4 - public", output_data="L4 - public")
+        if 'kpi' not in project.info:
+            project.info['kpi'] = 0.5
         project_repo.save(project)
         return project
 
